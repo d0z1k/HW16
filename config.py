72b9858e898044a1674c3cdb3ea806973dde0ce6
Xander <106524913+d0z1k@users.noreply.github.com>
2022-08-05 18:41:37 +0300

    json files added

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config('SQLALCHEMY_DATABASE_URI') = "sqlite:///test16HW.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///test16HW.db"
 
 
 db = SQLAlchemy(app)
